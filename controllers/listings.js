9b7cf1294ab24874a5462168785776202f642a86
Hemraj-singh-15705 <hemraj15705@gmial.com>
2024-08-12 18:50:28 +0530

    add new map feature

--- controllers/listings.js
+++ controllers/listings.js
@@ -1,4 +1,7 @@
 const listing = require("../Models/listing");
+const mbxGeocoding  = require('@mapbox/mapbox-sdk/services/geocoding');
+const mapToken = process.env.MAP_TOKEN;
+const geocodingClient = mbxGeocoding({ accessToken: mapToken });
 
 module.exports.index = async (req, res) => {
   const allListings = await listing.find({});
@@ -23,13 +26,22 @@ module.exports.showListing = async (req, res) => {
 };
 
 module.exports.createListing = async (req, res) => {
+   let response = await geocodingClient.forwardGeocode({
+    query: req.body.listing.location,
+    limit: 1,
+  }).send();
+
   let url = req.file.path;
   let filename = req.file.filename;
-
   const newListing = new listing(req.body.listing);
   newListing.owner = req.user._id;
   newListing.image = { url, filename };
-  await newListing.save();
+
+  newListing.geometry = response.body.features[0].geometry;
+
+  let savedListing = await newListing.save();
+  console.log(savedListing);
+
   req.flash("success", "New Listing Created !");
   res.redirect("/listings");
 };
@@ -56,7 +68,7 @@ module.exports.updateListing = async (req, res) => {
     let url = req.file.path;
     let filename = req.file.filename;
     Listing.image = { url, filename };
-    await listing.save();
+    await Listing.save();
   }
   req.flash("success", "Listing Updated !");
   res.redirect(`/listings/${id}`);
