ba388aa4bd8bc1917715335a56f42669087d24ed
Hemraj-singh-15705 <hemraj15705@gmial.com>
2024-08-15 17:56:16 +0530

    add new files

--- public/js/map.js
+++ public/js/map.js
@@ -1,17 +1,39 @@
-mapboxgl.accessToken = mapToken;
+// mapboxgl.accessToken = mapToken;
+
+// const map = new mapboxgl.Map({
+//   container: "map", // container ID
+//   center: Listing.geometry.coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
+//   zoom: 9, // starting zoom
+// });
+
+
+// const marker = new mapboxgl.Marker({ color: "red" })
+//   .setLngLat(Listing.geometry.coordinates) // geometry.listing.coordinates
+//   .setPopup(
+//     new mapboxgl.Popup({ offset: 25 }).setHTML(
+//       `<h4>${Listing.location}</h4><P>Exact Location will be provided after booking</P>`
+//     )
+//   )
+
+//   .addTo(map);
+
+
+
+
+  mapboxgl.accessToken = mapToken;
 
 const map = new mapboxgl.Map({
   container: "map", // container ID
-  center: Listing.geometry.coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
+  center: coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
   zoom: 9, // starting zoom
 });
 
 
 const marker = new mapboxgl.Marker({ color: "red" })
-  .setLngLat(Listing.geometry.coordinates) // geometry.listing.coordinates
+  .setLngLat(coordinates) // geometry.listing.coordinates
   .setPopup(
     new mapboxgl.Popup({ offset: 25 }).setHTML(
-      `<h4>${Listing.location}</h4><P>Exact Location will be provided after booking</P>`
+      `<h4></h4><P>Exact Location will be provided after booking</P>`
     )
   )
 
